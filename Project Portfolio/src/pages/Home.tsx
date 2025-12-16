070477d2ce6db9a2ecdbcf8bef7ff33462815816
Daniel Nguyen <daniel.nguyen.2345@gmail.com>
2025-12-16 10:43:21 -0600

    added background and section for

--- Project Portfolio/src/pages/Home.tsx
+++ Project Portfolio/src/pages/Home.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className='content-container'> 
+      About Me
+      <p>
+        I am a software developer with a passion for creating innovative solutions.
+        I have experience in full-stack development, working with technologies such as React, Node.js, and Python.
+        In my free time, I enjoy contributing to open-source projects and exploring new technologies. 
+        Feel free to take a look around my portfolio to see some of the projects I've worked on! 
+        I look forward to connecting with you.
+      </p>
+    </div>
   )
 }
 
