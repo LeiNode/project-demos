4510ba6575b5847229bd6bb8477506ae90e6abb5
Daniel Nguyen <daniel.nguyen.2345@gmail.com>
2026-03-30 17:20:18 -0500

    Update Home.tsx

--- Project Portfolio/src/pages/Home.tsx
+++ Project Portfolio/src/pages/Home.tsx
@@ -18,12 +18,18 @@ function Home() {
         <div className='content-container'> 
           Skills
           <p>
-            I am a software developer with a passion for creating innovative solutions.
-            I have experience in full-stack development, working with technologies such as React, Node.js, and Python.
-            In my free time, I enjoy contributing to open-source projects and exploring new technologies. 
-            Feel free to take a look around my portfolio to see some of the projects I've worked on! 
-            I look forward to connecting with you.
+            I have experience in a variety of programming languages and frameworks, including:
+            <ul>
+              <li>TypeScript (React, Node.js)</li>
+              <li>Python</li>
+              <li>SQL</li>
+            </ul>
           </p>
+          <p>
+            I am currrently learning about machine learning and data science, and I am excited to apply these skills to future projects.
+            Somethings that I am interested in exploring includes anomaly detection in time series data, natural language processing, and computer vision.   
+          </p>
+
         </div>
 
         <div className='content-container'> 
